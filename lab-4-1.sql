9a5c7b96de587ddbdd3a3fdd1f292ae6c7452175
David Gordon <david.gordon@kellogg.northwestern.edu>
2025-01-15 01:56:58 +0000

    completed lab 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,11 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT players.id, players.first_name, players.last_name, stats.hits, SUM(stats.hits)
+FROM stats INNER JOIN players ON players.id = stats.player_id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds";
+
+
+
 -- Expected result:
 -- 2935
 
